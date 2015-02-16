4bae1e10b84ba98b509a158973d22a61f8a9f213
Ma Lucheng <mlc880926@gmail.com>
2015-02-16 14:58:44 +0800

    display message when file changes
    update
    
    update
    
    display message when file changes

--- lib/guard/asciidoctor.rb
+++ lib/guard/asciidoctor.rb
@@ -1,8 +1,91 @@
+require 'asciidoctor'
 require 'guard/compat/plugin'
+require 'guard/asciidoctor/notifier'
 
 module Guard
 
   class Asciidoctor < Plugin
+
+    # Initializes a Guard plugin.
+    # Don't do any work here, especially as Guard plugins get initialized even if they are not in an active group!
+    #
+    # @param [Hash] options the custom Guard plugin options
+    # @option options [Array<Guard::Watcher>] watchers the Guard plugin file watchers
+    # @option options [Symbol] group the group this Guard plugin belongs to
+    # @option options [Boolean] any_return allow any object to be returned from a watcher
+    #
+    def initialize(options = {})
+      super
+    end
+
+    # Called once when Guard starts. Please override initialize method to init stuff.
+    #
+    # @raise [:task_has_failed] when start has failed
+    # @return [Object] the task result
+    #
+    def start
+      Compat::UI.info "method start"
+    end
+
+    # Called when `stop|quit|exit|s|q|e + enter` is pressed (when Guard quits).
+    #
+    # @raise [:task_has_failed] when stop has failed
+    # @return [Object] the task result
+    #
+    def stop
+      Compat::UI.info "method stop"
+    end
+
+    # Called when `reload|r|z + enter` is pressed.
+    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
+    #
+    # @raise [:task_has_failed] when reload has failed
+    # @return [Object] the task result
+    #
+    def reload
+      Compat::UI.info "method reload"
+    end
+
+    # Called when just `enter` is pressed
+    # This method should be principally used for long action like running all specs/tests/...
+    #
+    # @raise [:task_has_failed] when run_all has failed
+    # @return [Object] the task result
+    #
+    def run_all
+      Compat::UI.info "run all"
+    end
+
+    # Called on file(s) additions that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_additions has failed
+    # @return [Object] the task result
+    #
+    def run_on_additions(paths)
+      Compat::UI.info "run on additions"
+    end
+
+    # Called on file(s) modifications that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_modifications has failed
+    # @return [Object] the task result
+    #
+    def run_on_modifications(paths)
+      Compat::UI.info "run on modifications"
+    end
+
+    # Called on file(s) removals that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_removals has failed
+    # @return [Object] the task result
+    #
+    def run_on_removals(paths)
+      Compat::UI.info "run on removals"
+    end
+
 
   end
 
